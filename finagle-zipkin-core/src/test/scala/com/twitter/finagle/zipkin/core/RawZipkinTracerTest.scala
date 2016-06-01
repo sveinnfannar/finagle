ecc0a3efb98ce0ffa4feac671af4d9a5b7d60431
Sveinn Fannar <sveinnfannar@gmail.com>
2016-06-01 12:59:29 +0000

    Remove scribe specific tests already in finagle-zipkin and add test for flushing tracers

--- finagle-zipkin-core/src/test/scala/com/twitter/finagle/zipkin/core/RawZipkinTracerTest.scala
+++ finagle-zipkin-core/src/test/scala/com/twitter/finagle/zipkin/core/RawZipkinTracerTest.scala
@@ -4,7 +4,6 @@ import com.twitter.conversions.time._
 import com.twitter.finagle.service.TimeoutFilter
 import com.twitter.finagle.stats.NullStatsReceiver
 import com.twitter.finagle.tracing._
-import com.twitter.finagle.zipkin.thriftscala._
 import com.twitter.util._
 import java.net.{InetAddress, InetSocketAddress}
 
@@ -17,55 +16,16 @@ class RawZipkinTracerTest extends FunSuite {
 
   val traceId = TraceId(Some(SpanId(123)), Some(SpanId(123)), SpanId(123), None, Flags().setDebug)
 
-  class ScribeClient extends Scribe.FutureIface {
-    var messages: Seq[LogEntry] = Seq.empty[LogEntry]
-    var response: Future[ResultCode] = Future.value(ResultCode.Ok)
-    def log(msgs: Seq[LogEntry]): Future[ResultCode] = {
-      messages ++= msgs
-      response
+  class FakeRawZipkinTracer extends RawZipkinTracer(NullStatsReceiver) {
+    var spans: Seq[Span] = Seq.empty
+    override def sendSpans(xs: Seq[Span]): Future[Unit] = {
+      spans ++= xs
+      Future.Unit
     }
   }
 
-  test("formulate scribe log message correctly") {
-    val scribe = new ScribeClient
-    val tracer = new RawZipkinTracer(scribe, NullStatsReceiver)
-
-    val localEndpoint = Endpoint(2323, 23)
-    val remoteEndpoint = Endpoint(333, 22)
-
-    val annotations = Seq(
-      ZipkinAnnotation(Time.fromSeconds(123), "cs", localEndpoint),
-      ZipkinAnnotation(Time.fromSeconds(126), "cr", localEndpoint),
-      ZipkinAnnotation(Time.fromSeconds(123), "ss", remoteEndpoint),
-      ZipkinAnnotation(Time.fromSeconds(124), "sr", remoteEndpoint),
-      ZipkinAnnotation(Time.fromSeconds(123), "llamas", localEndpoint)
-    )
-
-    val span = Span(
-      traceId = traceId,
-      annotations = annotations,
-      _serviceName = Some("hickupquail"),
-      _name=Some("foo"),
-      bAnnotations = Seq.empty[BinaryAnnotation],
-      endpoint = localEndpoint)
-
-    val expected = LogEntry(
-      category = "zipkin",
-      message = "CgABAAAAAAAAAHsLAAMAAAADZm9vCgAEAAAAAAAAAHsKAAUAAAAAAAAAe" +
-        "w8ABgwAAAAFCgABAAAAAAdU1MALAAIAAAACY3MMAAMIAAEAAAkTBgACABcLAAMAAA" +
-        "ALaGlja3VwcXVhaWwAAAoAAQAAAAAHgpuACwACAAAAAmNyDAADCAABAAAJEwYAAgA" +
-        "XCwADAAAAC2hpY2t1cHF1YWlsAAAKAAEAAAAAB1TUwAsAAgAAAAJzcwwAAwgAAQAA" +
-        "AU0GAAIAFgsAAwAAAAtoaWNrdXBxdWFpbAAACgABAAAAAAdkFwALAAIAAAACc3IMA" +
-        "AMIAAEAAAFNBgACABYLAAMAAAALaGlja3VwcXVhaWwAAAoAAQAAAAAHVNTACwACAA" +
-        "AABmxsYW1hcwwAAwgAAQAACRMGAAIAFwsAAwAAAAtoaWNrdXBxdWFpbAAAAgAJAQA=\n")
-
-    tracer.logSpans(Seq(span))
-    assert(scribe.messages == Seq(expected))
-  }
-
-  test("send all traces to scribe") {
-    val scribe = new ScribeClient
-    val tracer = new RawZipkinTracer(scribe, NullStatsReceiver)
+  test("send spans when flushed") {
+    val tracer = new FakeRawZipkinTracer
 
     val localAddress = InetAddress.getByAddress(Array.fill(4) { 1 })
     val remoteAddress = InetAddress.getByAddress(Array.fill(4) { 10 })
@@ -85,23 +45,8 @@ class RawZipkinTracerTest extends FunSuite {
     tracer.record(Record(traceId, Time.fromSeconds(123), Annotation.ClientSend()))
     tracer.record(Record(traceId, Time.fromSeconds(123), Annotation.ClientRecv()))
 
-    // Note: Since ports are ephemeral, we can't hardcode expected message.
-    assert(scribe.messages.size == 1)
-  }
+    tracer.flush()
 
-  test("logSpan if a timeout occurs") {
-    val ann1 = Annotation.Message("some_message")
-    val ann2 = Annotation.Rpcname("some_service", "rpc_name")
-    val ann3 = Annotation.Message(TimeoutFilter.TimeoutAnnotation)
-
-    val scribe = new ScribeClient
-    val tracer = new RawZipkinTracer(scribe, NullStatsReceiver)
-
-    tracer.record(Record(traceId, Time.fromSeconds(1), ann1))
-    tracer.record(Record(traceId, Time.fromSeconds(2), ann2))
-    tracer.record(Record(traceId, Time.fromSeconds(3), ann3))
-
-    // scribe Log method is in java
-    assert(scribe.messages.size == 1)
+    assert(tracer.spans.length == 1)
   }
 }
