fed1e4aa870f6e57f9b9d11a4d3ab9006f53543e
Sveinn Fannar <sveinnfannar@gmail.com>
2016-06-06 16:37:59 +0000

    Improved producer config

--- finagle-zipkin-kafka/src/main/scala/com/twitter/finagle/zipkin/kafka/KafkaZipkinTracer.scala
+++ finagle-zipkin-kafka/src/main/scala/com/twitter/finagle/zipkin/kafka/KafkaZipkinTracer.scala
@@ -2,26 +2,31 @@ package com.twitter.finagle.zipkin.kafka
 
 import java.util.Properties
 
+import com.twitter.conversions.storage._
+import com.twitter.conversions.time._
 import com.twitter.finagle.stats.DefaultStatsReceiver
 import com.twitter.finagle.zipkin.core.SamplingTracer
+import com.twitter.util.{Duration, StorageUnit}
 import org.apache.kafka.clients.producer.{KafkaProducer, Producer}
 import com.twitter.finagle.zipkin.{initialSampleRate => sampleRateFlag, kafkaBootstrapServers => bootstrapServersFlag}
 
 object KafkaZipkinTracer {
   /**
     * @param kafkaBootstrapServers initial set of kafka brokers to connect to
+    * @param maxBufferSize max buffer size for the kafka producer
     */
-  private[kafka] def newProducer(kafkaBootstrapServers: String): Producer[Array[Byte], Array[Byte]] = {
+  private[kafka] def newProducer(
+    kafkaBootstrapServers: String,
+    maxBufferSize: StorageUnit = 5.megabytes
+  ): Producer[Array[Byte], Array[Byte]] = {
     val props: Properties = new Properties
     props.put("bootstrap.servers", kafkaBootstrapServers) // Initial brokers to connect to, rest of the cluster will be discovered
     props.put("key.serializer", "org.apache.kafka.common.serialization.ByteArraySerializer")
     props.put("value.serializer", "org.apache.kafka.common.serialization.ByteArraySerializer")
     props.put("acks", "1") // Ack after immediately writing to the leader
-//    props.put("buffer.memory", maxBufferSize.inBytes: java.lang.Long)
+    props.put("buffer.memory", maxBufferSize.inBytes: java.lang.Long)
     props.put("block.on.buffer.full", false: java.lang.Boolean) // Throw errors when buffer is full
-    //    props.put("max.block.ms", 0: java.lang.Integer) // Throw errors when buffer is full
     props.put("retries", 0: java.lang.Integer)
-//    props.put("request.timeout.ms", requestTimeout.inMilliseconds.toInt: java.lang.Integer)
     new KafkaProducer[Array[Byte], Array[Byte]](props)
   }
 }
